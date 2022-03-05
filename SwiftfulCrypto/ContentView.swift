0e8cd18b54b4280439ec8413ef77529ca7eac9c5
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-03-05 13:57:52 +0530

    First commit

---
+++ SwiftfulCrypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftfulCrypto
+//
+//  Created by MANAS VIJAYWARGIYA on 05/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by MANAS VIJAYWARGIYA on 05/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

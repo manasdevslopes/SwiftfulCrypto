e81d24360181beaef84d821c0acee59dcf5fea4d
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-03-05 14:45:11 +0530

    project setup

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by MANAS VIJAYWARGIYA on 05/03/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}

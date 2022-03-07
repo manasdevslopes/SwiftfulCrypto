f17e8e41aeb93770828c1bdc3f9f0c1499d40e01
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-03-07 18:27:49 +0530

    Coin Data Service Network call & reusable networking layer

---
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewModel.swift
+//  SwiftfulCrypto
+//
+//  Created by MANAS VIJAYWARGIYA on 07/03/22.
+//
+
+import Foundation
+import Combine
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        addSubscribers()
+    }
+    
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+    }
+}
